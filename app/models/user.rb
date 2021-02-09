63a1a1365c0b3d9178e3ab34ba1388dbaa6e9d40
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-09 18:37:54 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :password, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/}
+         validates :nickname, presence: true
+         validates :birthday, presence: true
+         validates :real_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} 
+         validates :real_family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+         validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :family_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+
 end
