f2b3366d8cc5dda3506d2adf2586ebc8a759c058
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-10 11:27:46 +0900

    パスワードのインスタンスで英数字を必ず生成　他

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname                {Faker::Games::SuperSmashBros.fighter}
     email                   {Faker::Internet.free_email}
-    password                {Faker::Internet.password(min_length: 6, mix_case: true)}
+    password                {'1a' +Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation   {password}
     birthday                {'1930-01-01'}
     real_first_name         {'陸太郎'}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -73,7 +73,8 @@ RSpec.describe User, type: :model do
       it 'パスワードには英数字どちらも含まれていないと登録できない' do
         @user.password = "aaaaAAAA"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password Password Include both letters and numbers")
+        binding.pry
+        expect(@user.errors.full_messages).to include("Password Include both letters and numbers")
       end
       it 'パスワードは６文字未満だと登録できない' do
         @user.password = "a1A"
@@ -88,82 +89,82 @@ RSpec.describe User, type: :model do
       it '名が半角英字だと登録できない' do
         @user.real_first_name = "rikutaro"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Real first name Real first name Full-width characters")
+        expect(@user.errors.full_messages).to include("Real first name Full-width characters")
       end
       it '名が全角英字だと登録できない' do
         @user.real_first_name = "ｒｉｋｕｔａｒｏ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Real first name Real first name Full-width characters")
+        expect(@user.errors.full_messages).to include("Real first name Full-width characters")
       end
       it '名が半角カタカナだと登録できない' do
         @user.real_first_name = "ﾘｸﾀﾛｳ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Real first name Real first name Full-width characters")
+        expect(@user.errors.full_messages).to include("Real first name Full-width characters")
       end
       it '姓が半角英字だと登録できない' do
         @user.real_family_name = "yamada"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Real family name Real family name Full-width characters")
+        expect(@user.errors.full_messages).to include("Real family name Full-width characters")
       end
       it '姓が全角英字だと登録できない' do
         @user.real_family_name = "ｙａｍａｄａ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Real family name Real family name Full-width characters")
+        expect(@user.errors.full_messages).to include("Real family name Full-width characters")
       end
       it '姓が半角カタカナだと登録できない' do
         @user.real_family_name = "ﾔﾏﾀﾞ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Real family name Real family name Full-width characters")
+        expect(@user.errors.full_messages).to include("Real family name Full-width characters")
       end
       it '名のフリガナが半角英字だと登録できない' do
         @user.first_furigana = "rikutaro"
         @user.valid?
-        expect(@user.errors.full_messages).to include("First furigana First furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("First furigana Full-width katakana characters")
       end
       it '名のフリガナが全角英字だと登録できない' do
         @user.first_furigana = "ＲＩＫＵＴＡＲＯ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("First furigana First furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("First furigana Full-width katakana characters")
       end
       it '名のフリガナが半角カタカナだと登録できない' do
         @user.first_furigana = "ﾘｸﾀﾛｳ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("First furigana First furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("First furigana Full-width katakana characters")
       end
       it '名のフリガナが漢字だと登録できない' do
         @user.first_furigana = "陸太郎"
         @user.valid?
-        expect(@user.errors.full_messages).to include("First furigana First furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("First furigana Full-width katakana characters")
       end
       it '名のフリガナがひらがなだと登録できない' do
         @user.first_furigana = "りくたろう"
         @user.valid?
-        expect(@user.errors.full_messages).to include("First furigana First furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("First furigana Full-width katakana characters")
       end
       it '姓のフリガナが半角英字だと登録できない' do
         @user.family_furigana = "yamada"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family furigana Family furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("Family furigana Full-width katakana characters")
       end
       it '姓のフリガナが全角英字だと登録できない' do
         @user.family_furigana = "ｙａｍａｄａ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family furigana Family furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("Family furigana Full-width katakana characters")
       end
       it '姓のフリガナが半角カタカナだと登録できない' do
         @user.family_furigana = "ﾔﾏﾀﾞ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family furigana Family furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("Family furigana Full-width katakana characters")
       end
       it '姓のフリガナが漢字だと登録できない' do
         @user.family_furigana = "山田"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family furigana Family furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("Family furigana Full-width katakana characters")
       end
       it '姓のフリガナがひらがなだと登録できない' do
         @user.family_furigana = "やまだ"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family furigana Family furigana Full-width katakana characters")
+        expect(@user.errors.full_messages).to include("Family furigana Full-width katakana characters")
       end
 
 
