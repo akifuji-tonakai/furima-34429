e30226ad5a33282bcbe2b5947ea67d37ae3a67a3
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-10 11:36:26 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
           validates :password, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: "Include both letters and numbers"}
           validates :nickname
           validates :birthday
-          validates :real_first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "Full-width characters"} 
-          validates :real_family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "Full-width characters"}
+          validates :real_first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "Full-width characters"} 
+          validates :real_family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "Full-width characters"}
           validates :first_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
           validates :family_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
          end
