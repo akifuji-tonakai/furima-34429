348927e68e0464cc599184ff834886215253d0fd
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-10 09:26:51 +0900

    バリデーションのエラーメッセージ変更
    , message: ""}とすることで、バリデーションに引っかかった時のエラーメッセージを変更できる

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :password, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/}
+         validates :password, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: "Password Include both letters and numbers"}
          validates :nickname, presence: true
          validates :birthday, presence: true
-         validates :real_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} 
-         validates :real_family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-         validates :family_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :real_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Real first name Full-width characters"} 
+         validates :real_family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Real family name Full-width characters"}
+         validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "First furigana Full-width katakana characters"}
+         validates :family_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Family furigana Full-width katakana characters"}
 
 end
