8b4908e659b064eb2f498dd2ea6f2a418203ef06
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-10 14:51:10 +0900

    コントローラー記述
    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :category_id, :status_id, :shipping_fee_id, :send_from_id, :shipping_days_id, :price).merge(user_id: current_user.id)
+  end
 end
