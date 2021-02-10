550a987e7c817fb9bd5aa46a0e2eddbdb4bb62ac
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-10 11:26:37 +0900

    with_optionsでpresenceをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :password, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: "Include both letters and numbers"}
-         validates :nickname, presence: true
-         validates :birthday, presence: true
-         validates :real_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"} 
-         validates :real_family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-         validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-         validates :family_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+         with_options presence: true do
+          validates :password, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: "Include both letters and numbers"}
+          validates :nickname
+          validates :birthday
+          validates :real_first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "Full-width characters"} 
+          validates :real_family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "Full-width characters"}
+          validates :first_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+          validates :family_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+         end
 
 end
