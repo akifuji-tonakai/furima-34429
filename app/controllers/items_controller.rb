e2715378daa3a2d60e4da808c5c58d4f56e71397
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-11 17:18:43 +0900

    コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :get_record, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order(created_at: "DESC")
@@ -17,9 +18,19 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
   
   def show
-    @item = Item.find(params[:id])
   end
 
 
@@ -27,5 +38,9 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :item_image, :introduction, :category_id, :status_id, :shipping_fee_id, :send_from_id, :shipping_days_id, :price).merge(user_id: current_user.id)
+  end
+  
+  def get_record
+    @item = Item.find(params[:id])
   end
 end
