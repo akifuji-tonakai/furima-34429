44fe7e4542a8b5d19ed59c1bf68ab50b6d112512
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-11 22:48:33 +0900

    user-itemアソシエーション修正
    商品は消しちゃいけないっぽいので

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_one_attached :item_image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
