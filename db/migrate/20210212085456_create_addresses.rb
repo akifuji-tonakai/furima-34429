02e62650841247230c4c76953f7c8d472c8bec6c
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-14 11:31:57 +0900

    migrationファイル修正

--- db/migrate/20210212085456_create_addresses.rb
+++ db/migrate/20210212085456_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer :send_from_id, null: false
       t.string :village_name, null: false
       t.string :village_number, null: false
-      t.string :building_detail, null: false
+      t.string :building_detail
       t.string :tele_number, null: false
       t.references :order, null: false, foreign_key: true
       t.timestamps
